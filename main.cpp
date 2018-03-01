d42d40b53118dc7eaffff18091f73e500575bfe6
Alexander Brock <alexander@lunar-orbit.de>
2018-03-01 16:01:23 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "json.hpp"
+#include "someclass.h"
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!" << endl;
+    SomeClass object;
+    return 0;
+}
---
+++ someclass.h
@@ -0,0 +1,12 @@
+#ifndef SOMECLASS_H
+#define SOMECLASS_H
+
+#include "json.hpp"
+
+class SomeClass
+{
+public:
+    SomeClass();
+};
+
+#endif // SOMECLASS_H
